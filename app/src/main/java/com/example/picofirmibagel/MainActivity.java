d495df4a47b4848b1ea31092bdd2f4d467dc0b72
cappoli <Cappoli567@gmail.com>
2019-11-07 11:42:14 -0500

    Pico Firmi Bagel

---
+++ app/src/main/java/com/example/picofirmibagel/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.picofirmibagel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    int guessA = 0;
+    int guessB = 0;
+    int guessC = 0;
+    int guessCheck = 0;
+    int correctAnswer = 0;
+
+    boolean picoYes;
+    boolean firmiYes;
+
+    String outcomeA;
+    String outcomeB;
+    String outcomeC;
+
+    String pico = "Pico";
+    String firmi = "Firmi";
+    String bagel = "Bagel";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void randomNumbers() {
+
+        a = (int) (Math.random() * (10));
+        b = (int) (Math.random() * (10));
+        c = (int) (Math.random() * (10));
+
+    }
+
+    public void comparator() {
+
+        firmiYes = false;
+        picoYes = false;
+
+
+        if (guessA == a || guessB == b || guessC == c) {
+            firmiYes = true;
+        }
+        if (guessA == b || guessA == c || guessB == a || guessB == c || guessC == a || guessC == b) {
+            picoYes = true;
+        }
+
+    }
+}
