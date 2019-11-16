9f1ab288a71fe1fc803b53cadafe6647561aed25
cappoli <Cappoli567@gmail.com>
2019-11-15 20:13:49 -0500

    added bagel outcome

--- app/src/main/java/com/example/picofirmibagel/MainActivity.java
+++ app/src/main/java/com/example/picofirmibagel/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,14 +25,17 @@ public class MainActivity extends AppCompatActivity {
     String inA;
     String inB;
     String inC;
+    String outCome;
 
     String pico = "Pico";
     String firmi = "Firmi";
     String bagel = "Bagel";
+    String tracker;
 
     EditText inputA;
     EditText inputB;
     EditText inputC;
+    TextView results;
 
 
     @Override
@@ -47,6 +51,11 @@ public class MainActivity extends AppCompatActivity {
         c = (int) (Math.random() * (10));
 
         System.out.println(" A: " + a + " B: " + b + " C: " + c);
+
+        tracker = "";
+
+        results = findViewById(R.id.textView2);
+        results.setText(tracker);
 
     }
 
@@ -75,6 +84,18 @@ public class MainActivity extends AppCompatActivity {
 
         if(guessA == a && guessB == b && guessC == c) {
             System.out.println("WINNER!!!");
+
+            a = (int) (Math.random() * (10));
+            b = (int) (Math.random() * (10));
+            c = (int) (Math.random() * (10));
+
+            System.out.println(" A: " + a + " B: " + b + " C: " + c);
+
+            tracker = "";
+
+            results = findViewById(R.id.textView2);
+            results.setText(tracker);
+
         }
         else {if (guessA == a || guessB == b || guessC == c) {
             firmiYes = true;
@@ -84,15 +105,28 @@ public class MainActivity extends AppCompatActivity {
             picoYes = true;
         }
 
+        outCome = "Result: " + guessA + " " + guessB + " " + guessC + "---";
+
         if(!picoYes && !firmiYes){
             System.out.println("Bagel");
+            outCome = outCome + "Bagel";
+
         }
         if (picoYes){
             System.out.println("Pico!");
+            outCome = outCome + " Pico";
         }
         if (firmiYes){
             System.out.println("Firmi");
+            outCome = outCome + " Firmi";
         }
+
+
+
+        tracker = outCome + System.getProperty("line.separator") + tracker;
+
+        results = findViewById(R.id.textView2);
+        results.setText(tracker);
 
         //Post both guess and results to the text view within the scroller
         //clear the previous guess
